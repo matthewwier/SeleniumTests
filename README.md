01dda404883062b2071be14ab781271663eb3a1d
Maciek <maciejjanwiercioch@gmail.com>
2019-06-05 16:48:27 +0200

    change config

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # selenium-tests
-Web application testing with Selenium and JUnit.
+Web application testing with Selenium 3.141.59 and JUnit 4.10. It's possible to run test suites on Chrome, Mozzila by specifying -Dbrowser parameter.
+
+Commands:
+
+mvn test // run all tests
+
+mvn -Dbrowser=Chrome clean package // run tests on Chrome browser
---
+++ src/test/java/TestLogin.java
@@ -0,0 +1,60 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.html5.WebStorage;
+import org.openqa.selenium.remote.Augmenter;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestLogin {
+
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp() throws IOException {
+        baseUrl = "http://localhost:5080";
+        java.io.InputStream is = this.getClass().getResourceAsStream("properties");
+        java.util.Properties p = new Properties();
+        p.load(is);
+        String browser = p.getProperty("browser");
+        if(browser.equals("Chrome")){
+            System.setProperty("webdriver.chrome.driver","./chromedriver" );
+            driver = new ChromeDriver();
+        }else{
+            System.setProperty("webdriver.gecko.driver", "./geckodriver");
+            DesiredCapabilities capabilities = DesiredCapabilities.firefox();
+            capabilities.setCapability("marionette", true);
+            driver = new FirefoxDriver();
+        }
+    }
+
+    @Test
+    public void testLogin() throws InterruptedException {
+        driver.get(baseUrl);
+
+        driver.findElement(By.cssSelector("input[type='email']")).sendKeys("adam@email.com");
+
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("input[type='password']")).sendKeys("adam_password");
+        Thread.sleep(1000);
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+
+        WebStorage webStorage = (WebStorage) new Augmenter().augment(driver);
+        String token = webStorage.getSessionStorage().getItem("token");
+        Assert.assertNotNull(token);
+    }
+
+    @After
+    public void teardown(){
+        driver.quit();
+    }
+
+}
